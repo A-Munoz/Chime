2a549f767865f74a8bb08fd0f8411cf048eb31e9
Luke Schwarting <jeluker2@gmail.com>
2019-04-11 00:00:09 -0400

    Fixed audio issue of not playing on heroku now playing src of audio tag that was created instead of sending to the server for the audio src it is already in code

--- server/view/main.js
+++ server/view/main.js
@@ -67,6 +67,7 @@
     canvas.height = window.innerHeight;
   }
 
+  /* Audio btns */
   const onSquareClicked = song => {
     console.log("colors clicked");
     console.log(song);
@@ -74,44 +75,62 @@
     audio.play();
   };
 
-  const songClick = count => {
-    if (count == 1) {
-      onSquareClicked("https://mysound.cad.rit.edu/audioFiles/knock.wav");
-    } else if (count == 2) {
-      onSquareClicked("https://mysound.cad.rit.edu/audioFiles/cork.mp3");
-    } else if (count == 3) {
-      onSquareClicked("https://mysound.cad.rit.edu/audioFiles/coins.wav");
-    } else if (count == 4) {
-      onSquareClicked("https://mysound.cad.rit.edu/audioFiles/keys.wav");
-    } else if (count == 5) {
-      onSquareClicked("https://mysound.cad.rit.edu/audioFiles/laugh.wav");
-    }
-  };
-
   const beat1 = () => {
-    songClick(1);
+    var src = $("#audio1").attr("src");
+    onSquareClicked(src);
   };
   const beat2 = () => {
-    songClick(2);
+    var src = $("#audio2").attr("src");
+    onSquareClicked(src);
   };
   const beat3 = () => {
-    songClick(3);
+    var src = $("#audio3").attr("src");
+    onSquareClicked(src);
   };
   const beat4 = () => {
-    songClick(4);
+    var src = $("#audio4").attr("src");
+    onSquareClicked(src);
   };
   const beat5 = () => {
-    songClick(5);
+    var src = $("#audio5").attr("src");
+    onSquareClicked(src);
   };
 
-  document.getElementById("btn1").addEventListener("click", beat1);
-  document.getElementById("btn2").addEventListener("click", beat2);
-  document.getElementById("btn3").addEventListener("click", beat3);
-  document.getElementById("btn4").addEventListener("click", beat4);
-  document.getElementById("btn5").addEventListener("click", beat5);
-
-  // for (let i = 0; i < colors.length; i++) {
-  //   console.log("in for");
-  //   colors[i].addEventListener("click", songClick);
-  // }
+  for (let i = 0; i < colors.length; i++) {
+    if (i == 0) {
+      var x = document.createElement("audio");
+      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/knock.wav");
+      x.setAttribute("id", "audio1");
+      document.getElementById("btn1").appendChild(x);
+      colors[i].addEventListener("click", beat1);
+    }
+    if (i == 1) {
+      var x = document.createElement("audio");
+      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/cork.mp3");
+      x.setAttribute("id", "audio2");
+      document.getElementById("btn2").appendChild(x);
+      colors[i].addEventListener("click", beat2);
+    }
+    if (i == 2) {
+      var x = document.createElement("audio");
+      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/coins.wav");
+      x.setAttribute("id", "audio3");
+      document.getElementById("btn3").appendChild(x);
+      colors[i].addEventListener("click", beat3);
+    }
+    if (i == 3) {
+      var x = document.createElement("audio");
+      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/keys.wav");
+      x.setAttribute("id", "audio4");
+      document.getElementById("btn4").appendChild(x);
+      colors[i].addEventListener("click", beat4);
+    }
+    if (i == 4) {
+      var x = document.createElement("audio");
+      x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/laugh.wav");
+      x.setAttribute("id", "audio5");
+      document.getElementById("btn5").appendChild(x);
+      colors[i].addEventListener("click", beat5);
+    }
+  }
 })();
