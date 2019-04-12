7357282ba0337e7542be1570f97c0bdce8f7e56c
PlatFleece <maverick.alpha4@gmail.com>
2019-04-12 13:42:37 -0400

    Formatting

--- server/view/client.js
+++ server/view/client.js
@@ -1,6 +1,6 @@
-const colors = document.getElementsByClassName("color");
+const colors = document.getElementsByTagName(`video`);
 const onSquareClicked = song => {
-  console.log("colors clicked");
+  console.log("videos clicked");
   console.log(song);
   var audio = new Audio(song);
   audio.play();
@@ -27,40 +27,42 @@ const beat5 = () => {
   onSquareClicked(src);
 };
 
+
 for (let i = 0; i < colors.length; i++) {
   if (i == 0) {
+    console.log(colors);
     var x = document.createElement("audio");
     x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/knock.wav");
     x.setAttribute("id", "audio1");
-    document.getElementById("btn1").appendChild(x);
+    document.getElementById("v1").appendChild(x);
     colors[i].addEventListener("click", beat1);
   }
   if (i == 1) {
     var x = document.createElement("audio");
     x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/cork.mp3");
     x.setAttribute("id", "audio2");
-    document.getElementById("btn2").appendChild(x);
+    document.getElementById("v2").appendChild(x);
     colors[i].addEventListener("click", beat2);
   }
   if (i == 2) {
     var x = document.createElement("audio");
     x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/coins.wav");
     x.setAttribute("id", "audio3");
-    document.getElementById("btn3").appendChild(x);
+    document.getElementById("v3").appendChild(x);
     colors[i].addEventListener("click", beat3);
   }
   if (i == 3) {
     var x = document.createElement("audio");
     x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/keys.wav");
     x.setAttribute("id", "audio4");
-    document.getElementById("btn4").appendChild(x);
+    document.getElementById("v4").appendChild(x);
     colors[i].addEventListener("click", beat4);
   }
   if (i == 4) {
     var x = document.createElement("audio");
     x.setAttribute("src", "https://mysound.cad.rit.edu/audioFiles/laugh.wav");
     x.setAttribute("id", "audio5");
-    document.getElementById("btn5").appendChild(x);
+    document.getElementById("v5").appendChild(x);
     colors[i].addEventListener("click", beat5);
   }
 }
