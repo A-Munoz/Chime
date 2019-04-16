077068bb4721a3a0f48070ff64d5178a8bf5001d
A.Munoz <A-Munoz@users.noreply.github.com>
2019-04-16 09:52:10 -0400

    cross-origin error, test fix

--- server/app.js
+++ server/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
 const app = express();
+const cors = require('cors');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
 const port = process.env.PORT || 3000;
-
+app.use(cors());
 // routing
 app.use('/', express.static(`${__dirname}/view`));
 app.use('/boardOne', express.static(`${__dirname}/view/soundboardOne.html`));
-
 function onConnection(socket) {
   // Updates Display
   socket.on('drawing', data => socket.broadcast.emit('drawing', data));
